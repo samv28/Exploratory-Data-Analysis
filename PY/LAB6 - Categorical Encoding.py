d3fee31a9728d1400d54565034fa924da1898759
sam <sameerkhatwani10@gmail.com.com>
2023-07-26 00:44:27 +0530

    eda

--- PY/LAB6 - Categorical Encoding.py
+++ PY/LAB6 - Categorical Encoding.py
@@ -6,3 +6,11 @@ df = pd.DataFrame({'team':['A','A','B','B','B','B','C','C'],
                    'points':[25,12,15,14,19,23,25,29]})
 
 print(df)
+
+#one hot encoding
+from sklearn.preprocessing import OneHotEncoder
+
+encoder = OneHotEncoder(handle_unknown='ignore')
+encoder_df = pd.DataFrame(encoder.fit_transform(df[['team']]).toarray())
+final_df = df.join(encoder_df)
+print(final_df)
