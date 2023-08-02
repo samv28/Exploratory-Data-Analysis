78aae35d7c3437b8ff7bb61b428e0f6a03ce7f58
sam <sameerkhatwani10@gmail.com.com>
2023-08-02 09:38:52 +0530

    edal

--- PY/LAB8 - Merging,Joining and Concatenating.py
+++ PY/LAB8 - Merging,Joining and Concatenating.py
@@ -33,4 +33,18 @@ right = pd.DataFrame({'key':['K0','K1','K2','K3'],
 print(left)
 print(right)
 
+#merging
+
 print(pd.merge(left,right,how='inner',on='key'))
+
+left = pd.DataFrame({'key1':['K0','K0','K1','K2'],
+                     'key2':['K0','K1','K0','K1'],
+                     'A':['A0','A1','A2','A3'],
+                     'B':['B0','B1','B2','B3']})
+right = pd.DataFrame({'key1':['K0','K1','K1','K2'],
+                      'key2':['K0','K0','K0','K0'],
+                      'C':['C0','C1','C2','C3'],
+                      'D':['D0','D1','D2','D3']})
+print(left)
+print(right)
+print(pd.merge(left,right,on=['key1','key2']))
